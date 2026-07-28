3af6c88b005c454a00cd22e8dab3ec3689f60cfd
atharvyadav72006-source <atharvyadav72006@gmail.com>
2026-07-28 04:25:09 +0000

    commit

---
+++ Q17.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,reverse=0,rem;
+    cout<<"Enter the number:";
+    cin>>n;
+
+    while(n!=0)
+    {
+    rem=n%10;
+    reverse=reverse*10+rem;
+    n=n/10;
+    }
+    cout<<"Reversed number="<<reverse;
+    
+    return 0;
+}
