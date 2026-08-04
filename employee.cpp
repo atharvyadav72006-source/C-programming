b5f2c0063da068699d9c8811cdc589574c267001
atharvyadav72006-source <atharvyadav72006@gmail.com>
2026-08-04 04:24:43 +0000

    commit

---
+++ employee.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+    int employeeID;
+    string name;
+    float salary;
+
+public:
+    void accept()
+    {
+        cout << "Enter Employee ID: ";
+        cin >> employeeID;
+
+        cout << "Enter Name: ";
+        cin >> name;
+
+        cout << "Enter Salary: ";
+        cin >> salary;
+    }
+
+    void display()
+    {
+        cout << "\nEmployee ID: " << employeeID;
+        cout << "\nName: " << name;
+        cout << "\nSalary: " << salary;
+    }
+};
+
+int main()
+{
+    Employee e;
+
+    e.accept();
+    e.display();
+
+    return 0;
+}
